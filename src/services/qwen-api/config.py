522dd6cb981471c8d5f27889425eb875b8b5bd78
kuyltep <vladpetlu91@gmail.com>
2025-11-19 22:36:15 +0300

    add basicly logic for qwen service

---
+++ src/services/qwen-api/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+class QwenAPIConfig(BaseSettings):
+  
+  qwen_api_key: SecretStr
+  qwen_api_base_url: str
+  qwen_model: str
+  
+  model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+  
+qwen_settings = QwenAPIConfig()
