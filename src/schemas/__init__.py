dc334cf07acdbb7018b7cf0656722586d168a08b
kuyltep <vladpetlu91@gmail.com>
2025-12-04 22:42:13 +0300

    add schemas for all models

---
+++ src/schemas/__init__.py
@@ -0,0 +1,17 @@
+from .base_message import MessageSchema
+from .sentiment_enum import SentimentEnum
+from .category_level_1_enum import CategoryLevel1Enum
+from .category_level_2_enum import CategoryLevel2Enum
+from .processed_message import MessageProcessedSchema
+from .emotion_enum import EmotionEnum
+from .categories_response import CategoriesResponse
+
+__all__ = [
+  "MessageSchema",
+  "SentimentEnum",
+  "CategoryLevel1Enum",
+  "CategoryLevel2Enum",
+  "MessageProcessedSchema",
+  "EmotionEnum",
+  "CategoriesResponse"
+]
