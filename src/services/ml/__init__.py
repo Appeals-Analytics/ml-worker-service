f53740d1e74606767e2b4bc0f0850809008a7dd6
kuyltep <vladpetlu91@gmail.com>
2025-12-04 22:42:56 +0300

    add base logic for ml pipeline and models

---
+++ src/services/ml/__init__.py
@@ -0,0 +1,5 @@
+from .pipeline import ml_pipeline
+from .base import BaseDetector
+from .language import LanguageDetector
+
+__all__ = ["ml_pipeline", "BaseDetector", "LanguageDetector"]
---
+++ src/services/ml/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+
+
+class MLSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    
+    language_model: str = "papluca/xlm-roberta-base-language-detection"
+    sentiment_model: str = "Niknik149/sentiment_true_cool"
+    emotion_model: str = "forxtears/airline-emotion-classifier"
+
+    models_cache_dir: str = "./cache/models"
+
+    num_threads: int = round(os.cpu_count() * 0.7)
+    num_interop_threads: int = 1
+
+ml_settings = MLSettings()
