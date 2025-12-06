80372b17d9eb8069d6f72b87549e5b8693e4698a
kuyltep <vladpetlu91@gmail.com>
2025-12-06 22:04:47 +0300

    update formating for ml and add clean text function

--- src/services/ml/language.py
+++ src/services/ml/language.py
@@ -3,20 +3,19 @@ from .config import ml_settings
 
 
 class LanguageResult:
-    
-    def __init__(self, lang_code: str, lang_score: float):
-        self.lang_code = lang_code
-        self.lang_score = lang_score
+  def __init__(self, lang_code: str, lang_score: float):
+    self.lang_code = lang_code
+    self.lang_score = lang_score
+
 
 class LanguageDetector(BaseDetector):
-    
-    @property
-    def model_name(self) -> str:
-        return ml_settings.language_model
-    
-    def detect(self, text: str) -> LanguageResult:
-        label, score = self._predict(text)
-        return LanguageResult(lang_code=label, lang_score=round(score, 4))
+  @property
+  def model_name(self) -> str:
+    return ml_settings.language_model
+
+  def detect(self, text: str) -> LanguageResult:
+    label, score = self._predict(text)
+    return LanguageResult(lang_code=label, lang_score=round(score, 4))
 
 
 language_detector = LanguageDetector()
